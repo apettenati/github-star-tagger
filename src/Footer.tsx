3d1b7fec3e7dc104b9bc0083f7d0d33134e81153
Amanda Pettenati <amanda@pette.net>
2021-04-28 21:41:19 -0400

    add footer

---
+++ src/Footer.tsx
@@ -0,0 +1,11 @@
+import { Link } from 'react-router-dom'
+import { FaGithub } from 'react-icons/fa'
+
+export function Footer() {
+
+  return (
+        <footer>
+          <p> Built by Amanda Pettenati | Source Code <a href="https://github.com/apettenati/github-star-tags">< FaGithub /></a> </p>
+        </footer>
+  )
+}
